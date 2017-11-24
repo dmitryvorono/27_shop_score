a1f62c87e401835b22e09b5278793833c2e97505
Dmitry <voronov-d88@yandex.ru>
2017-11-24 11:51:04 +0000

    add service score

--- app/views.py
+++ app/views.py
@@ -1,7 +1,35 @@
-from app import app
+from app import app, db, models
 from flask import render_template
+from flask import Response
+from datetime import timedelta, datetime
+import json
 
 
+@app.route('/api/score_information')
+def score_information():
+    returned_json_data = {
+        'count_completed_orders': get_completed_orders_on_period(),
+        'count_fulfillment_orders': get_fulfillment_orders(),
+    }
+    response = Response(json.dumps(returned_json_data),
+                    status=200,
+                    mimetype='application/json')
+    return response
+    
+    
+def get_completed_orders_on_period(days=1):
+    orders_query = db.session.query(models.Order)
+    orders_query = orders_query.filter((models.Order.created+timedelta(days))>datetime.now())
+    orders_query = orders_query.filter(models.Order.status == 'COMPLETED')
+    return orders_query.count()
+
+
+def get_fulfillment_orders():
+    orders_query = db.session.query(models.Order)
+    orders_query = orders_query.filter(models.Order.status == 'FULFILLMENT')
+    return orders_query.count()
+
+    
 @app.route('/')
 def score():
     return render_template('score.html')
